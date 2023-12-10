bca139ec28cb60351ead359c20cfadc3bed1dae8
derjacklive <71508487+derjacklive@users.noreply.github.com>
2023-12-10 01:39:06 +0100

    Big boeh Push: Added a test file, may become Index (will)

---
+++ docs/en-uk/js/indexhrefs.js
@@ -0,0 +1,7 @@
+document.getElementById("jnmlogo").addEventListener("click", function() {
+    window.location.replace("https://jnmprojects.github.io");
+});
+
+document.getElementById("name").addEventListener("click", function() {
+    window.location.replace("https://jnmprojects.github.io/robot-city");
+});

b84afe92a9b480a6afae7df88f0d189e5f38239b
derjacklive <71508487+derjacklive@users.noreply.github.com>
2024-01-17 19:28:27 +0100

    FINALY FIXED THIS BROKEN LOCAL try again 3/3

---
+++ NoPlus/Assets/Scripts/SceneLoader/LoadSelector.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor.PackageManager;
+
+public class LoadSelector : MonoBehaviour
+{
+    // Important Stuff Done In the Loading Head
+    public string IAm;
+    public string ILoad;
+    public Animator Animator;
+}
---
+++ NoPlus/Assets/Scripts/SceneLoader/LoadingHead.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadingHead : MonoBehaviour
+{
+    string target = StaticData.load;
+    string arrow = null;
+    Animator arrowimator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        string target = StaticData.load;
+        string arrow = null;
+        Animator arrowimator = null;
+
+        foreach (Transform child in transform)
+        {
+            LoadSelector childcomponent = child.GetComponent<LoadSelector>();
+            Animator childimator = childcomponent.Animator;
+
+            childimator.SetBool("selected", false);
+
+            Debug.Log(child.name + " has ben Reset for Loading");
+        }
+
+        foreach (Transform child in transform)
+        {
+            LoadSelector childcomponent = child.GetComponent<LoadSelector>();
+            Animator childimator = childcomponent.Animator;
+            string childis = childcomponent.IAm;
+            string childloads = childcomponent.ILoad;
+
+            if (childis == target)
+            {
+                arrow = childloads;
+                arrowimator = childimator;
+
+                Debug.Log(child.name + "is the Target");
+            }
+        }
+
+        if (arrow == null)
+        {
+            Debug.LogError("Fatal: Target hasnt been found.");
+            Application.Quit();
+        }
+
+        StartCoroutine(LoadScene(arrow, arrowimator));
+
+    }
+
+    IEnumerator LoadScene(string target, Animator targetimator)
+    {
+        targetimator.SetBool("selected", true);
+        yield return new WaitForSeconds(3);
+
+        AsyncOperation operation = SceneManager.LoadSceneAsync(target);
+        yield return operation;
+    }
+}

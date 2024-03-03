4d2dac7d6c00b436df8898585229fa8c36c35f98
derjacklive <71508487+derjacklive@users.noreply.github.com>
2024-03-03 23:35:49 +0100

    Fixed Api Rate Limit errors and Storybook build errors

--- noplus-web/src/components/apiaccess.ts
+++ noplus-web/src/components/apiaccess.ts
@@ -1,15 +1,24 @@
 import { Octokit } from "octokit";
+import { graphql } from "@octokit/graphql";
 
-const GHAPI = new Octokit({
-    auth: process.env.GH_PFP_TOKEN,
-    userAgent: "noplus-web",
+const graphqlWithAuth = graphql.defaults({
+    headers: {
+        authorization: `token ${process.env.GH_PFP_TOKEN}`,
+    },
 });
 
 async function parseGitAvatar(username: string) {
-    const response = await GHAPI.request('GET /users/{username}', {
-        username: username
-    });
-    return response.data.avatar_url;
-};
+    const query = `
+        query ($username: String!) {
+            user(login: $username) {
+                avatarUrl
+            }
+        }
+    `;
+
+    const response = await graphqlWithAuth(query, { username }) as any;
+
+    return response.user.avatarUrl;
+}
 
 export { parseGitAvatar };

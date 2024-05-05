73cc7b1c9fc6d0fa9b8b88204f24d42f63f9b38d
derjacklive <71508487+derjacklive@users.noreply.github.com>
2024-05-05 12:20:47 +0200

    MY GOD USING * AS FOO MAKES FB FAIL THE API CHECK AND EVERY ITEM NEEDS NEXT_PUBLIC_

--- noplus-web/src/components/firebase.ts
+++ noplus-web/src/components/firebase.ts
@@ -5,12 +5,12 @@ import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FB_APIKEY,
-  authDomain: process.env.FB_AUTHDOMAIN,
-  projectId: process.env.FB_PROJECTID,
-  storageBucket: process.env.FB_STORAGEBUCKET,
-  messagingSenderId: process.env.FB_MESSAGINGSENDERID,
-  appId: process.env.FB_APPID,
-  measurementId: process.env.FB_MEASUREMENTID
+  authDomain: process.env.NEXT_PUBLIC_FB_AUTHDOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FB_PROJECTID,
+  storageBucket: process.env.NEXT_PUBLIC_FB_STORAGEBUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FB_MESSAGINGSENDERID,
+  appId: process.env.NEXT_PUBLIC_FB_APPID,
+  measurementId: process.env.NEXT_PUBLIC_FB_MEASUREMENTID
 }
 
 const fbapp = initializeApp(firebaseConfig);
